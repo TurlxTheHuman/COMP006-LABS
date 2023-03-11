af19ead9983c651f1300ec7f7e6fa74edd5eebae
Turlx <contact@turlxthehuman.com>
2023-03-11 14:19:38 -0800

    LAB1

---
+++ PROG_FUND_1/PROG_FUND_1/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.tbRed = New System.Windows.Forms.TextBox()
+        Me.tbGreen = New System.Windows.Forms.TextBox()
+        Me.tbBlue = New System.Windows.Forms.TextBox()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lbTotal = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'tbRed
+        '
+        Me.tbRed.Location = New System.Drawing.Point(92, 79)
+        Me.tbRed.Name = "tbRed"
+        Me.tbRed.Size = New System.Drawing.Size(109, 23)
+        Me.tbRed.TabIndex = 0
+        '
+        'tbGreen
+        '
+        Me.tbGreen.Location = New System.Drawing.Point(92, 108)
+        Me.tbGreen.Name = "tbGreen"
+        Me.tbGreen.Size = New System.Drawing.Size(109, 23)
+        Me.tbGreen.TabIndex = 1
+        '
+        'tbBlue
+        '
+        Me.tbBlue.Location = New System.Drawing.Point(92, 137)
+        Me.tbBlue.Name = "tbBlue"
+        Me.tbBlue.Size = New System.Drawing.Size(109, 23)
+        Me.tbBlue.TabIndex = 2
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(45, 212)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 3
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(126, 212)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(56, 82)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 15)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Red:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 111)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Green:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(53, 140)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(33, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Blue:"
+        '
+        'lbTotal
+        '
+        Me.lbTotal.AutoSize = True
+        Me.lbTotal.Location = New System.Drawing.Point(126, 177)
+        Me.lbTotal.Name = "lbTotal"
+        Me.lbTotal.Size = New System.Drawing.Size(0, 15)
+        Me.lbTotal.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkSeaGreen
+        Me.ClientSize = New System.Drawing.Size(253, 285)
+        Me.Controls.Add(Me.lbTotal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.tbBlue)
+        Me.Controls.Add(Me.tbGreen)
+        Me.Controls.Add(Me.tbRed)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Balloons"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents tbRed As TextBox
+    Friend WithEvents tbGreen As TextBox
+    Friend WithEvents tbBlue As TextBox
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lbTotal As Label
+End Class
---
+++ PROG_FUND_1/PROG_FUND_1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        lbTotal.Text = tbRed.Text + 0 + tbGreen.Text + 0 + tbBlue.Text
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lbTotal.Text = ""
+        tbRed.Text = ""
+        tbGreen.Text = ""
+        tbBlue.Text = ""
+    End Sub
+End Class
