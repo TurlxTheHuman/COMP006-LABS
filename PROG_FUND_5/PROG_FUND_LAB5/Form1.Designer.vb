3d0005842a8b755c3e25285fe33da551754f2313
Turlx <contact@turlxthehuman.com>
2023-03-11 14:20:20 -0800

    LAB5

---
+++ PROG_FUND_5/PROG_FUND_LAB5/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnSubmit = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        lstAges = New ListBox()
+        txtName = New TextBox()
+        txtTicket = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        lblOutput = New Label()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' btnSubmit
+        ' 
+        btnSubmit.Location = New Point(12, 209)
+        btnSubmit.Name = "btnSubmit"
+        btnSubmit.Size = New Size(75, 23)
+        btnSubmit.TabIndex = 0
+        btnSubmit.Text = "Submit"
+        btnSubmit.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(129, 209)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(75, 23)
+        btnClear.TabIndex = 1
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(242, 209)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(75, 23)
+        btnExit.TabIndex = 2
+        btnExit.Text = "Exit"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' lstAges
+        ' 
+        lstAges.FormattingEnabled = True
+        lstAges.ItemHeight = 15
+        lstAges.Location = New Point(222, 12)
+        lstAges.Name = "lstAges"
+        lstAges.Size = New Size(95, 124)
+        lstAges.TabIndex = 3
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(12, 30)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(204, 23)
+        txtName.TabIndex = 4
+        ' 
+        ' txtTicket
+        ' 
+        txtTicket.Location = New Point(12, 72)
+        txtTicket.Name = "txtTicket"
+        txtTicket.Size = New Size(61, 23)
+        txtTicket.TabIndex = 5
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(12, 54)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(209, 15)
+        Label1.TabIndex = 6
+        Label1.Text = "Have you had a ticket in the past year?"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(79, 75)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(48, 15)
+        Label2.TabIndex = 7
+        Label2.Text = "(Y or N)"' 
+        ' lblOutput
+        ' 
+        lblOutput.AutoSize = True
+        lblOutput.Location = New Point(12, 144)
+        lblOutput.Name = "lblOutput"
+        lblOutput.Size = New Size(0, 15)
+        lblOutput.TabIndex = 8
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(12, 12)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(64, 15)
+        Label3.TabIndex = 9
+        Label3.Text = "Full Name:"' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(329, 253)
+        Controls.Add(Label3)
+        Controls.Add(lblOutput)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(txtTicket)
+        Controls.Add(txtName)
+        Controls.Add(lstAges)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnSubmit)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lstAges As ListBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtTicket As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblOutput As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ PROG_FUND_5/PROG_FUND_LAB5/Form1.vb
@@ -0,0 +1,65 @@
+﻿Option Strict On
+Option Infer Off
+Option Explicit On
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ages As Integer = 16
+        Do While ages <> 101
+            lstAges.Items.Add(ages)
+            ages = ages + 1
+        Loop ' Loops from ages 16 - 100
+        lstAges.SelectedIndex = 0
+    End Sub
+
+    Private Sub txtTicket_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtTicket.KeyPress
+        If (e.KeyChar).ToString.ToUpper = "Y" Then ' Uppercase + Lowercase Y
+            e.Handled = False
+        ElseIf (e.KeyChar).ToString.ToUpper = "N" Then ' Uppercase + Lowercase N
+            e.Handled = False
+        ElseIf e.KeyChar = Convert.ToChar(8) Then ' Backspace
+            e.Handled = False
+        Else
+            e.Handled = True ' Handle everything not a Y,N,Backspace
+        End If
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtName.Text = ""
+        txtTicket.Text = ""
+        lblOutput.Text = ""
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim selectedAge As String = lstAges.SelectedItem.ToString
+        Dim fullName As String = txtName.Text
+        Dim hadTicket As Boolean = False
+
+        If txtTicket.Text.ToUpper = "Y" Then
+            hadTicket = True
+        End If
+
+
+        If selectedAge < "30" Then
+            If hadTicket = False Then
+                lblOutput.Text = "Great! You qualify for insurance for $45"
+            ElseIf hadTicket = True Then
+                lblOutput.Text = "Your rate for insurance for $85"
+            End If
+        End If
+
+        If selectedAge >= "30" Then
+            If hadTicket = False Then
+                lblOutput.Text = "Great! You qualify for insurance for $30"
+            ElseIf hadTicket = True Then
+                lblOutput.Text = "Your rate for insurance for $85"
+            End If
+        End If
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+End Class
