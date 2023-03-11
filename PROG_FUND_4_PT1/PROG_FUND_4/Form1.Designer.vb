6254eb0a167bc1b8315bbf973761aa31aecacfb1
Turlx <contact@turlxthehuman.com>
2023-03-11 14:20:04 -0800

    LAB4-PT1

---
+++ PROG_FUND_4_PT1/PROG_FUND_4/Form1.Designer.vb
@@ -0,0 +1,165 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtYears = New System.Windows.Forms.TextBox()
+        Me.txtSales = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnQuit = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(158, 66)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 23)
+        Me.txtName.TabIndex = 0
+        '
+        'txtYears
+        '
+        Me.txtYears.Location = New System.Drawing.Point(158, 92)
+        Me.txtYears.Name = "txtYears"
+        Me.txtYears.Size = New System.Drawing.Size(50, 23)
+        Me.txtYears.TabIndex = 1
+        '
+        'txtSales
+        '
+        Me.txtSales.Location = New System.Drawing.Point(158, 121)
+        Me.txtSales.Name = "txtSales"
+        Me.txtSales.Size = New System.Drawing.Size(50, 23)
+        Me.txtSales.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(50, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(208, 32)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Bonus Calculator"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 69)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 15)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "First and Last Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(120, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Years With Company:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 124)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(140, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Sales for Month (Dollars):"
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(12, 201)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubmit.TabIndex = 7
+        Me.btnSubmit.Text = "Submit"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(109, 201)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnQuit
+        '
+        Me.btnQuit.Location = New System.Drawing.Point(202, 201)
+        Me.btnQuit.Name = "btnQuit"
+        Me.btnQuit.Size = New System.Drawing.Size(75, 23)
+        Me.btnQuit.TabIndex = 9
+        Me.btnQuit.Text = "Quit"
+        Me.btnQuit.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.Location = New System.Drawing.Point(29, 162)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(0, 15)
+        Me.lblOutput.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(289, 303)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnQuit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtSales)
+        Me.Controls.Add(Me.txtYears)
+        Me.Controls.Add(Me.txtName)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtYears As TextBox
+    Friend WithEvents txtSales As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnQuit As Button
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ PROG_FUND_4_PT1/PROG_FUND_4/Form1.vb
@@ -0,0 +1,44 @@
+﻿Option Strict On
+Option Infer Off
+Option Explicit On
+
+
+Public Class Form1
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+
+        Dim Name As String = txtName.Text
+        Dim Years As Integer
+        Dim Sales As Integer
+        Integer.TryParse(txtSales.Text, Sales)
+
+        If txtName.Text = "" Or txtSales.Text = "" Or txtYears.Text = "" Then
+            lblOutput.Text = "You Are Missing An Input"
+        Else
+            If Integer.TryParse(txtYears.Text, Years) Then
+                If Years >= 5 And Sales >= 100000 Then
+                    lblOutput.Text = Name + " You Get A $1000 Bonus!"
+                Else
+                    lblOutput.Text = Name + " You Won't Get A Bonus"
+                End If
+            Else
+                lblOutput.Text = "Please have a year value > 0"
+            End If
+        End If
+    End Sub
+
+    Private Sub txtSales_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSales.KeyPress
+        If Not Char.IsNumber(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtName.Text = ""
+        txtYears.Text = ""
+        txtSales.Text = ""
+    End Sub
+
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Application.Exit()
+    End Sub
+End Class
